8f701857ef86a34b32d1211ec0c2e8f752da9eb9
Gennady Egupov <ludvig31@yandex.ru>
2020-01-20 15:29:54 +0300

    Adding a Production Version

--- gulp/getPugUtil.js
+++
@@ -1,6 +0,0 @@
-export default () => {
-  const util = require('../src/pug/local/util');
-  delete require.cache[require.resolve('../src/pug/local/util')]; // prevent module caching
-
-  return util;
-};
--- gulp/task/cleanDist.js
+++
@@ -1,5 +0,0 @@
-import del from 'del';
-
-const cleanDist = () => del(['dist']);
-
-export default cleanDist;
